a5b98c6e2a497b9215f176561d4188cd6c4f459a
Seiji Story <sstory@sfu.ca>
2020-07-06 12:08:19 -0700

    Diceware script modifications

--- .bin/diceware.sh
+++ .bin/diceware.sh
@@ -4,6 +4,7 @@ if [ $# -ge 1 ]; then
 else
 	RANGE=6
 fi
+echo $RANGE
 PASS=""
 for i in `seq 1 $RANGE`; do
 	A=`grep -m1 -ao '[1-6]' /dev/urandom | sed s/0/10/ | head -n1`
@@ -12,7 +13,7 @@ for i in `seq 1 $RANGE`; do
 	D=`grep -m1 -ao '[1-6]' /dev/urandom | sed s/0/10/ | head -n1`
 	E=`grep -m1 -ao '[1-6]' /dev/urandom | sed s/0/10/ | head -n1`
 	F="$A$B$C$D$E"
-	WORD=`grep $F $HOME/.diceware | tail -c+7`
+	WORD=`grep $F $HOME/.diceware | /bin/tail -c+7`
 	PASS="$PASS $WORD"
 done
 echo $PASS
